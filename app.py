15ff30068dceea72d236027dc9f7c5c45be543c0
Gazz-w <105929513+Gazz-w@users.noreply.github.com>
2024-10-19 15:42:47 -0300

    'about-us' page search implemented

--- app.py
+++ app.py
@@ -6,6 +6,69 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains import LLMChain
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.pydantic_v1 import BaseModel, Field
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import requests
+
+# Exctract links with beautiful soup
+
+
+def extract_links(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    links = []
+    for link in soup.find_all('a', href=True):
+        link_text = link.get_text(strip=True)
+        href = link['href']
+        full_url = urljoin(url, href)
+        links.append({'text': link_text, 'url': full_url})
+    return links
+
+# Function to find the about us link
+
+
+def find_about_us_link(user_api_key, url):
+    llm = ChatOpenAI(
+        model_name="gpt-4o-mini",
+        temperature=0,
+        openai_api_key=user_api_key,
+    )
+
+    # Extract links from the webpage
+    links = extract_links(url)
+
+    # Prepare the list of links as a string
+    links_str = '\n'.join(
+        [f"{i+1}. Text: {link['text']}, URL: {link['url']}" for i, link in enumerate(links)])
+
+    # Define the prompt
+
+    prompt_template = """
+    You are to analyze the following list of webpage links and identify the URL of the 'About Us' page. The 'About Us' page may also be named 'About', 'Sobre Nós', 'Quem Somos', or similar variations in Portuguese. Provide only the URL of the correct page.
+    Links: {links}
+    {format_instructions}
+    """
+
+    prompt = PromptTemplate(
+        template=prompt_template,
+        input_variables=["links", "format_instructions"]
+    )
+
+    class UrlExtract(BaseModel):
+        url: str = Field(
+            description="The exact URL of the 'About Us' or similar page")
+
+    parser = JsonOutputParser(pydantic_object=UrlExtract)
+
+    chain = prompt | llm | parser
+
+    return chain.invoke({
+        "links": links_str,
+        "format_instructions": parser.get_format_instructions()
+    })
 
 #  Retrieves documents from the web using the provided URL.
 
@@ -43,14 +106,14 @@ def create_chain(user_api_key):
     Your task is to research all the information about these company for a sales team.
     Company: {context}
     Output: {question} 
-    Must contain: The company name; {must_contain}
-    Must not contain: {must_not_contain}                                      
+    Must_contain: The company name; {must_contain}
+    Article_structure example: {article_structure}                                      
      """
 
     prompt = PromptTemplate(
         template=prompt_template,
         input_variables=["context", "question",
-                         "must_contain", "must_not_contain"]
+                         "must_contain", "article_structure"]
     )
 
     llm_chain = LLMChain(
@@ -78,14 +141,25 @@ def main():
     st.sidebar.header('Search Preferences')
     must_contain = st.sidebar.text_input(
         '2- Enter the information that must be in the article')
-    must_not_contain = st.sidebar.text_input(
-        '3- Enter the information that you do not want in the article')
+    article_structure = st.sidebar.text_area(
+        '3- Enter a model or structure that you want included in the article')
 
     url = st.text_input('Enter the URL')
 
     if not url:
         st.warning('Pleas enter the URL')
         return
+
+    first_search = find_about_us_link(user_api_key, url)
+
+    about_url = first_search["url"]
+    if not about_url:
+        st.write(f" not found, contiuing without the about us page...")
+        about_docs = ""
+    else:
+        st.write(f"About-us page: {about_url}")
+        st.success('creating the article...')
+        about_docs = get_documents_from_web(about_url)
 
     docs = get_documents_from_web(url)
 
@@ -94,17 +168,20 @@ def main():
 
     retriever = vector_store.as_retriever(search_kwargs={"k": 3})
 
-    question = "produce a concise, informative article about this prospect for the sales team"
+    question = "produce a concise, informative article about this company for the sales team that {must_contain}"
 
     relevant_docs = retriever.get_relevant_documents(question)
 
     context = "\n\n".join([doc.page_content for doc in relevant_docs])
 
+    about_context = about_docs
+
     response = chain.invoke({
         "context": context,
+        "about-page": about_context,
         "question": question,
         "must_contain": must_contain,
-        "must_not_contain": must_not_contain,
+        "article_structure": article_structure,
     })
 
     article = response["text"]
@@ -113,7 +190,7 @@ def main():
     st.success('finished!')
 
     # to debug the response:
-    # st.write(response)
+    st.write(response)
 
 
 if __name__ == "__main__":
